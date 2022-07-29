c8065b4e97b1923c5d56f4bf2e4f6b2e1a703d8e
Ammad Akhtar <ammadakhtar@live.com>
2022-07-29 12:33:02 +0500

    added project strtucture

---
+++ Git-Trendo/Application/Coordinator/AppFlowCoordinator.swift
@@ -0,0 +1,8 @@
+//
+//  AppFlowCoordinator.swift
+//  Git-Trendo
+//
+//  Created by Ammad Akhtar on 29/07/2022.
+//
+
+import Foundation
---
+++ Git-Trendo/Application/DIContainer/AppDIContainer.swift
@@ -0,0 +1,8 @@
+//
+//  AppDIContainer.swift
+//  Git-Trendo
+//
+//  Created by Ammad Akhtar on 29/07/2022.
+//
+
+import Foundation
